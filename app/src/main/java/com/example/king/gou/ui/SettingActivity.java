02643fdd1add832b94d520ab129e7e6890afdcf6
king <1027956041@qq.com>
2017-06-06 00:09:01 +0800

    新资源

--- app/src/main/java/com/example/king/gou/ui/SettingActivity.java
+++ app/src/main/java/com/example/king/gou/ui/SettingActivity.java
@@ -1,16 +1,45 @@
 package com.example.king.gou.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.king.gou.R;
+import com.example.king.gou.ui.settingfragment.UpdataPwdActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id._back)
+    ImageView Back;
+    @BindView(R.id.Setting_UpdataPwd)
+    RelativeLayout SettingUpdataPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+        SettingUpdataPwd.setOnClickListener(this);
+    }
+
+    public void StartA(Class cls) {
+        startActivity(new Intent(this, cls));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Setting_UpdataPwd:
+                StartA(UpdataPwdActivity.class);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/king/gou/ui/settingfragment/UpdataPwdActivity.java
@@ -0,0 +1,16 @@
+package com.example.king.gou.ui.settingfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.king.gou.R;
+
+
+public class UpdataPwdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updata_pwd);
+    }
+}
