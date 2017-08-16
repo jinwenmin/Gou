37686b29a40ca5d5695b549419737012fe8f7e1e
qq7066 <706642112@qq.com>
2017-08-16 17:12:02 +0800

    投注

--- app/src/main/java/com/example/king/gou/ui/GameCenterActivity.java
+++ app/src/main/java/com/example/king/gou/ui/GameCenterActivity.java
@@ -127,7 +127,7 @@ public class GameCenterActivity extends AutoLayoutActivity implements HttpEngine
         RetrofitService.getInstance().getSwitchGameList(this, gid);
         RetrofitService.getInstance().getBettingSync(this, gid);
         RetrofitService.getInstance().getBettingDrawHistory(this, gid);
-        View view = LayoutInflater.from(this).inflate(R.layout.item_g1_optional2, null, false);
+        View view = LayoutInflater.from(this).inflate(R.layout.item_g1_beijing10_dragon_tiger, null, false);
         GameCenterLinear.addView(view);
         initClick();
         initData();
